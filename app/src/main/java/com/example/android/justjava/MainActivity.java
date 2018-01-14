04fe85fdccb7f7d56fb9f51fae94979240e8f5ab
Jonathan Hardison <jmh@jonathanhardison.com>
2018-01-14 01:48:42 -0600

    Lesson 11 completion.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,13 +9,15 @@
 package com.example.android.justjava;
 
 
-
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import java.text.NumberFormat;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -23,6 +25,8 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
     int quantity = 2;
     int priceOfCoffee = 5;
+    int priceOfChocolate = 2;
+    int priceOfWhippedCream = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,15 +41,35 @@ public class MainActivity extends AppCompatActivity {
     public void submitOrder(View view) {
 
 
-        String orderMessage = createOrderSummary(quantity);
 
-        displayMessage(orderMessage);
+        CheckBox whippedTextView = (CheckBox) findViewById(R.id.whiptopping_check_box);
+        boolean whippedChecked = whippedTextView.isChecked();
+
+        CheckBox chocolateTextView = (CheckBox) findViewById(R.id.chocolate_check_box);
+        boolean chocolateChecked = chocolateTextView.isChecked();
+
+        EditText usersNameEditText = (EditText) findViewById(R.id.nameofuser);
+
+        String orderMessage = createOrderSummary(quantity, whippedChecked, chocolateChecked, usersNameEditText.getText().toString());
+
+        composeEmail(getString(R.string.composeemailsubject, usersNameEditText.getText().toString()), orderMessage);
 
 
     }
 
-    private int calculatePrice(){
-        return quantity * priceOfCoffee;
+    private int calculatePrice(boolean chocolate, boolean whippedcream){
+        int coffeePrice = priceOfCoffee;
+
+        if(chocolate){
+            coffeePrice += priceOfChocolate;
+        }
+        if(whippedcream){
+            coffeePrice += priceOfWhippedCream;
+        }
+
+        coffeePrice = coffeePrice * quantity;
+
+        return coffeePrice;
     }
 
     /**
@@ -57,31 +81,29 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText("" + inputNumber);
     }
 
-    /**
-     * This method displays the given quantity value on the screen
-     * @param number
-     */
-    private void displayPrice(int number){
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(NumberFormat.getCurrencyInstance().format(number));
 
+    public void displayToast(String message){
+        Context context = getApplicationContext();
+        CharSequence text = message;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
-
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
-
     /**
      * Increment quantity
      * @param view
      */
     public void increment(View view){
-        quantity = quantity +1;
-        displayQuantity(quantity);
+        if(quantity < 100) {
+            quantity = quantity + 1;
+            displayQuantity(quantity);
+        }
+        else{
+            //add toast here.
+            displayToast(getString(R.string.morethan100toast));
+            return;
+        }
     }
 
     /**
@@ -89,8 +111,15 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void decrement(View view){
-        quantity = quantity -1;
-        displayQuantity(quantity);
+        if(quantity > 1) {
+            quantity = quantity - 1;
+            displayQuantity(quantity);
+        }
+        else{
+            //toast for oops
+            displayToast(getString(R.string.lessthan1toast));
+            return;
+        }
     }
 
     /**
@@ -98,17 +127,29 @@ public class MainActivity extends AppCompatActivity {
      * @param orderPrice
      * @return
      */
-    public String createOrderSummary(int orderPrice){
+    public String createOrderSummary(int orderQty, boolean whippedTopping, boolean chocolateTopping, String nameOfUser){
 
-        String nameOfUser = "Kaptain Kunal";
-        int price = calculatePrice();
 
-        String priceMessage = "Name: " + nameOfUser + "\n";
-        priceMessage = priceMessage + "Quantity: " + orderPrice + "\n";
-        priceMessage = priceMessage + "Total: $" + price + "\n";
-        priceMessage = priceMessage + "Thank You!";
+        int price = calculatePrice(chocolateTopping, whippedTopping);
+
+        String priceMessage = getString(R.string.composebodyline1, nameOfUser) + "\n";
+        priceMessage = priceMessage + getString(R.string.composebodyline2, whippedTopping) + "\n";
+        priceMessage = priceMessage + getString(R.string.composebodyline3, chocolateTopping) + "\n";
+        priceMessage = priceMessage + getString(R.string.composebodyline4, orderQty) + "\n";
+        priceMessage = priceMessage + getString(R.string.composebodyline5, price) + "\n";
+        priceMessage = priceMessage + getString(R.string.composebodyline6);
 
         return priceMessage;
+    }
+
+    public void composeEmail(String subject, String message) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
 
